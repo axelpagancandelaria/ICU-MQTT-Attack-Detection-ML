0bc3c738dc14c7ff30fe7f0ce71be4705b818650
Edgar De Leon <edg.deleon27@gmail.com>
2026-04-27 01:24:10 -0500

    Added comments and cleaned up a bit of the files

--- Data-Processing-Pipeline/train_RandomForest.py
+++ Data-Processing-Pipeline/train_RandomForest.py
@@ -1,13 +1,22 @@
+#This script trains and evaluates a Random Forest ML using the ICU
+#dataset. It then prints the model accuracy and a confusion matrix
+#with a report.
+
 import pandas as pd
 from sklearn.model_selection import train_test_split
 from sklearn.ensemble import RandomForestClassifier
 from sklearn.metrics import classification_report, confusion_matrix, accuracy_score
 
-df = pd.read_csv("icu_ml_ready.csv")
+#Load the dataset
+df = pd.read_csv("../Traffic_Data_Sets/icu_ml_ready.csv")
 
+#This separates the features from the target label and drops the
+#capture timestamps to avoid the ML from using those as part of detection
 X = df.drop(columns=["label", "frame.time_epoch"])
 y = df["label"]
 
+#Splits the dataset into training and testing, while keeping
+#a class balance in both sets
 X_train, X_test, y_train, y_test = train_test_split(
     X, y,
     test_size=0.2,
@@ -15,15 +24,20 @@ X_train, X_test, y_train, y_test = train_test_split(
     stratify=y
 )
 
+#Creates the ML
 rf = RandomForestClassifier(
     n_estimators=100,
     random_state=42,
     class_weight="balanced"
 )
+
+#Training ML
 rf.fit(X_train, y_train)
 
+#Use the model to predict the labels for the testing set
 y_pred = rf.predict(X_test)
 
+#Prints the matrix, and the results
 print("Accuracy:", accuracy_score(y_test, y_pred))
 print("\nConfusion Matrix:")
 print(confusion_matrix(y_test, y_pred))
@@ -31,6 +45,7 @@ print(confusion_matrix(y_test, y_pred))
 print("\nClassification Report:")
 print(classification_report(y_test, y_pred))
 
+#Shows the most important features used by the model
 importances = pd.Series(rf.feature_importances_, index=X.columns).sort_values(ascending=False)
 print("\nTop Feature Importances:")
 print(importances.head(10))
